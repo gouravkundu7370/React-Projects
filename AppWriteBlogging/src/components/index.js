fbad9f3ea3178b8966dd29fed4d308563a5519c6
Gourav Kundu <gouravkundu2902@gmail.com>
2023-11-24 20:52:04 +0530

    Added Components

--- AppWriteBlogging/src/components/index.js
+++ AppWriteBlogging/src/components/index.js
@@ -1,4 +1,23 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Select from "./Select";
+import Login from "./Login";
+import Signup from "./Signup";
+import PostCard from "./PostCard";
 
-export { Header, Footer };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Select,
+  PostCard,
+  Signup,
+  Login,
+};
